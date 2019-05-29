ff31e8b045717ac0f384649fe4efb6058570ba23
freeintelligence <free04381@gmail.com>
2019-05-29 17:49:17 -0400

    Detalle..

--- src/flags/flag.class.ts
+++ src/flags/flag.class.ts
@@ -117,7 +117,7 @@ export class Flag {
       name += `-${this.getMainAlias()}`
     }
     if(this.getMainName() || !this.isRequired()) {
-      name = (name.length ? ' '+name+' ' : '')+'['
+      name = (name.length ? name+' ' : '')+'['
 
       if(this.getMainName()) {
         name += `--${this.getMainName()}`
