068fdf5b3c9526212ca4c10a17775d3121943b1b
freeintelligence <free04381@gmail.com>
2019-05-30 12:10:39 -0400

    Se elimina el strict mode on flags para cuando una bandera es requerida ya que independiente de esta configuracion, si es requerida y no tiene valor, entonces debe mostrar el error de igual manera.

--- src/scheduler.class.ts
+++ src/scheduler.class.ts
@@ -161,7 +161,7 @@ export class Scheduler {
       if(flag.options.type !== 'array') {
         const resource_flag = from.find(e => (!e.used && e.type == 'flag' && flag.getNames().indexOf(e.name) !== -1) || (!e.used && e.type == 'flag-alias' && flag.getAliases().indexOf(e.name) !== -1))
 
-        if(!resource_flag && typeof flag.getDefault() == 'undefined' && this.config.strict_mode_on_flags) {
+        if(!resource_flag && typeof flag.isRequired()) {
           throw new RequiredFlagValueError(command, flag)
         }
 
