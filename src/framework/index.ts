6334ec40622e184fd524c1bfbb4ef26dfc241c26
freeintelligence <free04381@gmail.com>
2019-06-02 20:01:36 -0400

    Se cambia la 'base' del paquete para utilizar el formato de los paquetes Nalv.

---
+++ src/framework/index.ts
@@ -0,0 +1,11 @@
+export * from './interfaces'
+export * from './inspector'
+export * from './flags'
+export * from './commands'
+export * from './extras'
+export * from './helper'
+export * from './configure'
+export * from './middletasks'
+export * from './errors'
+
+export * from './scheduler.class'
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-export * from './interfaces'
-export * from './inspector'
-export * from './flags'
-export * from './commands'
-export * from './extras'
-export * from './helper'
-export * from './configure'
-export * from './middletasks'
-export * from './errors'
-
-export * from './scheduler.class'
+// all exports
+export * from './framework'
